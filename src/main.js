bf3717d540124eb053910a71fda018ccecbf8716
hgy <huanggengyou@sjtu.edu.cn>
2023-06-14 16:36:02 +0800

    可用于本地测试

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from "@/router"
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import axios from "axios";
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = "/api"
+const app = createApp(App);
+app.use(VueAxios, axios);
+app.use(ElementPlus);
+app.use(router);
+app.mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+module.exports ={
+  devServer: {
+    // host: 'http://120.26.103.180/',
+    host: 'localhost',
+    port: 8080,  //没被占用，可以使用的端口
+    open: true,
+    proxy: {
+      '/api':{
+        target: 'http://localhost:5000', //接口域名
+        // target: 'http://120.26.103.180:5000', //接口域名
+        changeOrigin: true,             //是否跨域
+        secure: false,                   //是否https接口
+        pathRewrite: {                  //路径重置
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
